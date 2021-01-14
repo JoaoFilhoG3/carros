a40160c2567e35ec3c058028b67ae49ec4cfcf8f
João Filho <joao.filho.g3@gmail.com>
2021-01-13 22:15:31 -0300

    Configurando formulário da login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
   final _txtLogin = TextEditingController();
   final _txtSenha = TextEditingController();
 
@@ -13,29 +19,64 @@ class LoginPage extends StatelessWidget {
   }
 
   Widget _buildBody() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          _buildTextFormField("Login", "Digite o login", controller: _txtLogin),
-          SizedBox(
-            height: 10,
-          ),
-          _buildTextFormField("Senha", "Digite sua senha",
-              controller: _txtSenha, password: true),
-          SizedBox(
-            height: 20,
-          ),
-          _buildButton(
-            "Login",
-            () {
-              String login = _txtLogin.text;
-              String senha = _txtSenha.text;
-            },
-          ),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            _buildTextFormField(
+              "Login",
+              "Digite o login",
+              controller: _txtLogin,
+              validator: _validateLogin,
+              textInputAction: TextInputAction.next,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _buildTextFormField(
+              "Senha",
+              "Digite sua senha",
+              controller: _txtSenha,
+              password: true,
+              validator: _validateSenha,
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            _buildButton(
+              "Login",
+              () {
+                if (!_formKey.currentState.validate()) {
+                  return;
+                }
+                String login = _txtLogin.text;
+                String senha = _txtSenha.text;
+              },
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  String _validateLogin(String texto) {
+    if (texto.isEmpty) {
+      return "Campo login obrigatório!";
+    }
+    return null;
+  }
+
+  String _validateSenha(String texto) {
+    if (texto.isEmpty) {
+      return "Campo senha obrigatório!";
+    }
+    if (texto.length < 8) {
+      return "A senha deve ter pelo menos 8 caracteres!";
+    }
+    return null;
   }
 
   Container _buildButton(String text, Function onPressed()) {
@@ -55,9 +96,19 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  TextFormField _buildTextFormField(String label, String hint,
-      {bool password = false, TextEditingController controller}) {
+  TextFormField _buildTextFormField(
+    String label,
+    String hint, {
+    bool password = false,
+    TextEditingController controller,
+    FormFieldValidator<String> validator,
+    TextInputType keyboardType,
+    TextInputAction textInputAction,
+  }) {
     return TextFormField(
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      validator: validator,
       controller: controller,
       obscureText: password,
       style: TextStyle(
